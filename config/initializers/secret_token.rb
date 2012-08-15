68e9a15fd4e7494f14f0ca2c3ab41d3d1d0f4a87
Ali <alistair.edmunds@gmail.com>
2012-08-15 11:28:57 +0100

    Created application using Refinery CMS 2.0.6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AliCms::Application.config.secret_token = 'e51992bcf0459de44322ccc6a18deb77a1c5a1137c210c2d7443dc7768e856fc205ac02fe3a85a92becf27bc3d55a4aec00afde6dd5d920cf1dc29e21d93416b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AliCms::Application.config.session_store :cookie_store, key: '_ali_cms_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AliCms::Application.config.session_store :active_record_store
